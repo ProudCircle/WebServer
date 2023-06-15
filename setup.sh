c8943563bb8b49df53e1ccac8d20deb143fc2dfc
illyum <90023277+itzilly@users.noreply.github.com>
2023-06-15 16:26:52 -0600

    Add setup script
    This script will attempt to install all dependencies for hosting ruby on rails

---
+++ setup.sh
@@ -0,0 +1,10 @@
+# Adding Node.js repository
+curl -sL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
+# Adding Yarn repository
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo add-apt-repository ppa:chris-lea/redis-server
+# Refresh our packages list with the new repositories
+sudo apt-get update -y
+# Install our dependencies for compiiling Ruby along with Node.js and Yarn
+sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev software-properties-common libffi-dev dirmngr gnupg apt-transport-https ca-certificates redis-server redis-tools nodejs yarn
